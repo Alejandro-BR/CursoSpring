d85e4dc69d71ae9491dcca330ce74605572930dc
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-05-29 10:02:46 +0200

    Inicilizar springboot-aop

---
+++ springboot-aop/src/test/java/com/alejandro/curso/springboot/app/aop/springboot_aop/SpringbootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.curso.springboot.app.aop.springboot_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
