c1206296aef9b52620d76e1d1af91feed9edf58a
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-05-29 08:07:59 +0200

    Inicializar springboot-horario

---
+++ springboot-horario/src/main/java/com/alejandro/curso/springboot/calendar/interceptor/springboot_horario/SpringbootHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.curso.springboot.calendar.interceptor.springboot_horario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHorarioApplication.class, args);
+	}
+
+}
---
+++ springboot-horario/src/test/java/com/alejandro/curso/springboot/calendar/interceptor/springboot_horario/SpringbootHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.curso.springboot.calendar.interceptor.springboot_horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
