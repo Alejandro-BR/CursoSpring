79c84dfa9fba530c0aeebfd8f3d44cfb331cef4a
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-05-30 12:05:16 +0200

    Importar insert

---
+++ springboot-jpa/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO persons (name, lastname, programing_language) VALUES ('Andres', 'Guzman', 'Java');
+INSERT INTO persons (name, lastname, programing_language) VALUES ('Alejandro', 'Barrionuevo', 'Rosado');
+INSERT INTO persons (name, lastname, programing_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programing_language) VALUES ('John', 'Dow', 'JavaScript');
+INSERT INTO persons (name, lastname, programing_language) VALUES ('Maria', 'Roe', 'Java');
+INSERT INTO persons (name, lastname, programing_language) VALUES ('Josefa', 'Rae', 'Java');
