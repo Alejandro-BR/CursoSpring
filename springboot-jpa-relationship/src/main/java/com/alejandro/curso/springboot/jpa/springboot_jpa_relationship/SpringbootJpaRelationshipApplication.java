02befd79f2a30f2f7b5622f37d6fa31d306b73ce
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2024-06-03 08:59:04 +0200

    Inicializar springboot-jpa-relationship

---
+++ springboot-jpa-relationship/src/main/java/com/alejandro/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.curso.springboot.jpa.springboot_jpa_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+	}
+
+}
---
+++ springboot-jpa-relationship/src/test/java/com/alejandro/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.curso.springboot.jpa.springboot_jpa_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
